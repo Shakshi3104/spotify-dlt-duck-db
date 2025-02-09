12b678bbd8584e4127e416819d7d49017e276865
Shakshi3104 <51497924+Shakshi3104@users.noreply.github.com>
2025-02-09 17:57:14 +0900

    [update] Update a little

--- check_duck_db.py
+++ check_duck_db.py
@@ -2,7 +2,7 @@ import duckdb
 
 
 if __name__ == "__main__":
-    duckdb_connection = './spotify_api_example.duckdb'
+    duckdb_connection = 'spotify_api_example.duckdb'
 
     spotify_data = duckdb.connect(duckdb_connection)
     rel = spotify_data.sql(
