3a5fe0a0b8ae3909e2a05dacf074ae918742ace2
Shakshi3104 <51497924+Shakshi3104@users.noreply.github.com>
2025-02-09 17:17:49 +0900

    [add] Add the code to check DuckDB

---
+++ check_duck_db.py
@@ -0,0 +1,14 @@
+import duckdb
+
+
+if __name__ == "__main__":
+    duckdb_connection = './spotify_api_example.duckdb'
+
+    spotify_data = duckdb.connect(duckdb_connection)
+    rel = spotify_data.sql(
+        """
+        select name, popularity, album__name, external_urls__spotify
+        from naniwa_top_tracks.top_tracks
+        """
+    )
+    rel.show()
